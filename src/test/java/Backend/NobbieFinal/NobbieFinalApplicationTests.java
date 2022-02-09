95e03421168a708eba68fc0e0ffba452d7c362fe
Saskia Bousardt <saskia.bousardt@gmail.com>
2022-02-09 16:51:42 +0100

    project setup

---
+++ src/test/java/Backend/NobbieFinal/NobbieFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.NobbieFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobbieFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
